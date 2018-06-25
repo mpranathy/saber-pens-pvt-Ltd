62b24606945d47d22eae5e5dc6edd67ebabbcea8
Pranathy <pranathymahendran@gmail.com>
2018-06-25 20:43:48 +0530

    saber pens
    a startup company

---
+++ start up/js/custom.js
@@ -0,0 +1,17 @@
+$(function(){
+     new WOW().init();
+    
+});
+
+
+$(function(){
+     $("#rooms").owlCarousel({
+         items:3,
+         autoplay:true,
+         smartSpeed:700,
+         loop:true,
+    
+         
+     });
+    
+});
